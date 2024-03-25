216ec57396688227c4e2477721ffa57c8b8b6233
talvaneramos <talvanesilvaramos@gmail.com>
2024-03-25 17:11:04 -0300

    Criado as entidades Dependente e Funcionario

---
+++ Projeto.Domain/Entities/Dependente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Domain.Entities
+{
+    public class Dependente
+    {
+        public int IdDependente { get; set; }
+        public string? Nome { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public int IdFuncionario { get; set; }
+
+        public Funcionario? Funcionario { get; set; }
+    }
+}
---
+++ Projeto.Domain/Entities/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Domain.Entities
+{
+    public class Funcionario
+    {
+        public int IdFuncionario { get; set; }
+        public string? Nome { get; set; }
+        public string? Cpf { get; set; }
+        public DateTime? DataAdmissao { get; set; }
+        public decimal? Salario { get; set; }
+
+        public List<Dependente>? Dependentes { get; set; }
+    }
+}
