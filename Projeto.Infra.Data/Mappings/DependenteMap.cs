06d623e50ac3ea0128c3980b18c1b81985cb5a33
talvaneramos <talvanesilvaramos@gmail.com>
2024-03-25 17:58:37 -0300

    Feito o mapeamento das entidades para serem geradas através do ambiente EF. Criado o bd ProjetoDDD e colocado o caminho de acesso no arquivo appsettings.json

---
+++ Projeto.Infra.Data/Mappings/DependenteMap.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Projeto.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Infra.Data.Mappings
+{
+    internal class DependenteMap : IEntityTypeConfiguration<Dependente>
+    {
+        public void Configure(EntityTypeBuilder<Dependente> builder)
+        {
+            builder.ToTable("Dependente");
+
+            builder.HasKey(d => d.IdDependente);
+
+            builder.Property(d => d.IdDependente)
+                .HasColumnName("IdDependente");
+
+            builder.Property(d => d.Nome)
+                .HasColumnName("Nome")
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(d => d.DataNascimento)
+                .HasColumnName("DataNascimento")
+                .HasColumnType("Date")
+                .IsRequired();
+
+            builder.Property(d => d.IdFuncionario)
+                .HasColumnName("IdFuncionario")
+                .IsRequired();
+
+            builder.HasOne(d => d.Funcionario)
+                .WithMany(f => f.Dependentes)
+                .HasForeignKey(d => d.IdFuncionario);
+        }
+    }
+}
---
+++ Projeto.Infra.Data/Mappings/FuncionarioMap.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Projeto.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Infra.Data.Mappings
+{
+    public class FuncionarioMap : IEntityTypeConfiguration<Funcionario>
+    {
+        public void Configure(EntityTypeBuilder<Funcionario> builder)
+        {
+            builder.ToTable("Funcionario");
+
+            builder.HasKey(f => f.IdFuncionario);
+
+            builder.Property(f => f.IdFuncionario)
+                .HasColumnName("IdFuncionario");
+
+            builder.Property(f => f.Nome)
+                .HasColumnName("Nome")
+                .HasMaxLength(150)
+                .IsRequired();
+            
+            builder.Property(f => f.Cpf)
+                .HasColumnName("Cpf")
+                .HasMaxLength(15)
+                .IsRequired();
+
+            builder.Property(f => f.DataAdmissao)
+                .HasColumnName("DataAdmissao")
+                .HasColumnType("date")
+                .IsRequired();
+
+            builder.Property(f => f.Salario)
+                .HasColumnName("Salario")
+                .HasColumnType("decimal(18, 2)")
+                .IsRequired();
+                
+        }
+    }
+}
