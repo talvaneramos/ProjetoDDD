246b657d7e4b521a0b28afd0a586ff734982f8ab
talvaneramos <talvanesilvaramos@gmail.com>
2024-03-25 17:16:47 -0300

    Criada a interface genérica e as interfaces específicas que herdarão da interface genérica.

---
+++ Projeto.Domain/Contracts/IDependenteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Projeto.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Domain.Contracts
+{
+    public interface IDependenteRepository : IBaseRepository<Dependente>
+    {
+    }
+}
---
+++ Projeto.Domain/Contracts/IFuncionarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using Projeto.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Domain.Contracts
+{
+    public interface IFuncionarioRepository : IBaseRepository<Funcionario>
+    {
+        Funcionario GetByCpf(string cpf);
+    }
+}
