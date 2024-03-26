b4051bd968e48439ada007323ffc8982d4b929ce
talvaneramos <talvanesilvaramos@gmail.com>
2024-03-26 06:58:37 -0300

    Feito a classe de mapeamento das entidades, configuração do EF e BD.

---
+++ Projeto.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto.Domain.Entities;
+using Projeto.Infra.Data.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Infra.Data.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Funcionario> Funcionario { get; set; }
+        public DbSet<Dependente> Dependente { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new FuncionarioMap());
+            modelBuilder.ApplyConfiguration(new DependenteMap());
+        }
+    }
+}
