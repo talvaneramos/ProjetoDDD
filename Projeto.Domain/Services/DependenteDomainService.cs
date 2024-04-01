9cc9306ccd91005a6eef7d5f411c023ddfd0cfc4
talvaneramos <talvanesilvaramos@gmail.com>
2024-04-01 15:24:52 -0300

    Dependente: Não é permitido cadastrar mais do que 3 dependentes por funcionário.

--- Projeto.Domain/Services/DependenteDomainService.cs
+++ Projeto.Domain/Services/DependenteDomainService.cs
@@ -12,11 +12,41 @@ namespace Projeto.Domain.Services
     public class DependenteDomainService : BaseDomainService<Dependente>, IDependenteDomainService
     {
         private readonly IDependenteRepository _dependenteRepository;
+        private readonly IFuncionarioRepository _funcionarioRepository;
 
-        public DependenteDomainService(IDependenteRepository dependenteRepository)
+        public DependenteDomainService(IDependenteRepository dependenteRepository, IFuncionarioRepository funcionarioRepository)
             : base(dependenteRepository)
         {
             _dependenteRepository = dependenteRepository;
+            _funcionarioRepository = funcionarioRepository;
+        }
+
+        public override void Add(Dependente obj)
+        {
+            var quantidade = _funcionarioRepository.CountDependentes(obj.IdFuncionario);
+
+            if (quantidade >= 3)
+            {
+                throw new Exception("Funcionário atingiu o limite de 3 Dependentes. ");
+            }
+            else
+            {
+                _dependenteRepository.Add(obj);
+            }
+        }
+
+        public override void Update(Dependente obj)
+        {
+            var quantidade = _funcionarioRepository.CountDependentes(obj.IdFuncionario);
+
+            if (quantidade >= 3)
+            {
+                throw new Exception("Funcionário atingiu o limite de 3 Dependentes. ");
+            }
+            else
+            {
+                _dependenteRepository.Update(obj);
+            }
         }
     }
 }
