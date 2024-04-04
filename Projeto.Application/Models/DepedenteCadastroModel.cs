14d704e5fc7296b94cc9a0f2a309ca2c0351235a
talvaneramos <talvanesilvaramos@gmail.com>
2024-04-04 16:03:49 -0300

    Criação das Dependente na Models de aplicação e sua dataannotations

---
+++ Projeto.Application/Models/DepedenteCadastroModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Application.Models
+{
+    public class DepedenteCadastroModel
+    {
+        [Required(ErrorMessage = " Por favor, informe o nome do dependente. ")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = " Por favor, informe a data de nascimento do dependente. ")]
+        public string? DataNascimento { get; set; }
+
+        [Required(ErrorMessage = " Por favor, informe id do Funcionário. ")]
+        public string? IdFuncionario { get; set; }
+    }
+}
---
+++ Projeto.Application/Models/DependenteConsultaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Application.Models
+{
+    public class DependenteConsultaModel
+    {
+        public string? IdDependente { get; set; }
+        public string? Nome { get; set; }
+        public string? DataNascimento { get; set; }
+        public string? IdFuncionario { get; set; }
+    }
+}
