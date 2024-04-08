33b2054e4a886c1bae1f6d1fed3cb6dbc183caa2
talvaneramos <talvanesilvaramos@gmail.com>
2024-04-08 18:37:14 -0300

    Criação dos controladores e a biblioteca do swagger.

---
+++ Projeto.Presentation.Api/Controllers/DependenteController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto.Presentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DependenteController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) 
+        {
+            return Ok();
+        }    
+        
+    }
+}
--- Projeto.Presentation.Api/Program.cs
+++ Projeto.Presentation.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API para controle de Funcionários e Dependentes",
+        Description = "Projeto Desenvolvido em NET CORE 7.0 API com EF e DDD.",        
+        Contact = new OpenApiContact
+        {
+            Name = "Talvane Ramos",
+            Email = "talvanesilvaramos@gmail.com"
+        },        
+    });
+});
 
 var app = builder.Build();
 
@@ -19,6 +35,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Projeto");
+    options.RoutePrefix = string.Empty;
+});
 
 app.MapControllers();
 
