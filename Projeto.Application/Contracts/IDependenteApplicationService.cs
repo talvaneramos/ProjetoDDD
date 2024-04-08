80d8c89226a6561785f16e78b2bd73d6d8b05add
talvaneramos <talvanesilvaramos@gmail.com>
2024-04-08 16:07:48 -0300

    Criada as interfaces applicationService

---
+++ Projeto.Application/Contracts/IDependenteApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using Projeto.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Application.Contracts
+{
+    public interface IDependenteApplicationService
+    {
+        void Add(DepedenteCadastroModel model);
+        void Update(DependenteEdicaoModel model);
+        void Delete(int id);
+        List<DependenteConsultaModel> GetAll();
+        DependenteConsultaModel GetById(int id);
+    }
+}
---
+++ Projeto.Application/Contracts/IFuncionarioApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using Projeto.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Application.Contracts
+{
+    public interface IFuncionarioApplicationService
+    {
+        void Add(FuncionarioCadastroModel model);
+        void Update(FuncionarioEdicaoModel model);
+        void Delete(int id);
+        List<FuncionarioConsultaModel> GetAll();
+        FuncionarioConsultaModel GetById(int id);
+    }
+}
