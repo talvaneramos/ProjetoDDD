b71a0b1ae1e7b55d3f09f88e9b4cdbe4443e33ed
talvaneramos <talvanesilvaramos@gmail.com>
2024-04-09 13:55:45 -0300

    Realizado o mapeamentos das camadas(repositório, domínio e aplicação) e configuração da string do banco na program.cs

--- Projeto.Presentation.Api/Program.cs
+++ Projeto.Presentation.Api/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Projeto.Application.Contracts;
+using Projeto.Application.Services;
+using Projeto.Domain.Contracts.Repositories;
+using Projeto.Domain.Contracts.Services;
+using Projeto.Domain.Services;
+using Projeto.Infra.Data.Contexts;
+using Projeto.Infra.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +17,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configuração da string de conexão do EF
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer
+(builder.Configuration.GetConnectionString("ProjetoDDD")));
+
+//camada de repositório
+builder.Services.AddTransient<IFuncionarioRepository, FuncionarioRepository>();
+builder.Services.AddTransient<IDependenteRepository, DependenteRepository>();
+
+//Camada de domínio
+builder.Services.AddTransient<IFuncionarioDomainService, FuncionarioDomainService>();
+builder.Services.AddTransient<IDependenteDomainService, DependenteDomainService>();
+
+//Camada de aplicação
+builder.Services.AddTransient<IFuncionarioApplicationService, FuncionarioApplicationService>();
+builder.Services.AddTransient<IDependenteApplicationService, DependenteApplicationService>();
+
 
 builder.Services.AddSwaggerGen(options =>
 {
